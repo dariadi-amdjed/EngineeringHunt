7f3c9c6545a8bfedaf18c987136222131a3bbd58
dariadi-amdjed <daryadiamdjed@gmail.com>
2026-09-17 21:53:37 +0100

    Add Vercel Web Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { Layout } from '@/components/Layout';
 import { HomePage } from '@/pages/HomePage';
 import { ExplorePage } from '@/pages/ExplorePage';
@@ -11,6 +12,7 @@ import { AboutPage } from '@/pages/AboutPage';
 export default function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
